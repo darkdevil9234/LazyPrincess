f691bc5c95b5200c0cf59b9497bb06ac0338fa8b
darkdevil9234 <130627650+darkdevil9234@users.noreply.github.com>
2023-04-15 20:20:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://smmehta7367:password1234@cluster0.j17rl6i.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://smmehta7367:password1234@cluster0.9k0egrb.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cinemaa_Boxoffice")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
